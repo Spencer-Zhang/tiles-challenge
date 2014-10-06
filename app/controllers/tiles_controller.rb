918bab16f3ef913b46be387cce3a57436dc60a61
Spencer Zhang <syz4@cornell.edu>
2014-10-06 16:06:02 -0500

    Added slim, js, and scss files for index page

---
+++ app/controllers/tiles_controller.rb
@@ -0,0 +1,5 @@
+class TilesController < ApplicationController
+  def index
+    p "test"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ TilesChallenge::Application.routes.draw do
 
 
 
-
+root 'tiles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
